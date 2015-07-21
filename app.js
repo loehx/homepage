af2edc9d7b6116e574eb700fddf51753932a1e9b
Alexander Löhn <alexloehn@yahoo.de>
2015-07-21 15:02:54 +0000

    Use Mustache instead of Handlebars

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mustacheExpress = require('mustache-express');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -11,8 +12,10 @@ var users = require('./routes/users');
 var app = express();
 
 // view engine setup
+app.engine('mustache', mustacheExpress());
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.set('view engine', 'mustache');
+
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
