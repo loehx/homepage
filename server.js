7d7657de6277efbf20ff657413bb1b6a162efca5
Alexander Löhn <alexloehn@gmail.com>
2015-11-15 12:59:38 +0100

    update

--- server.js
+++ server.js
@@ -1,9 +1,6 @@
 var app = require('./app')
+var port = process.env.PORT || 27015
 
-
-var port = process.env.OPENSHIFT_NODEJS_PORT || 27015
-var host = process.env.OPENSHIFT_NODEJS_IP || 'localhost'
-
-app.listen(port, host, function() {
-    console.log('Homepage started at http://' + host + ':' + port);
+app.listen(port, function() {
+    console.log('Homepage started at http://localhost:' + port);
 })
